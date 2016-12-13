41cd7fc19e2a1501945fa154eb87d4fd80493ac8
ramboli1986 <ramboli1986@gmail.com>
2016-12-13 12:39:55 -0500

    Create Dictionary Structure

--- WeShot/WeShot/Classes/Others/AppDelegate.h
+++ WeShot/WeShot/Classes/Others/AppDelegate.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  AppDelegate.h
 //  WeShot
 //
 //  Created by bo LI on 12/13/16.
@@ -8,7 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
 
 
 @end
